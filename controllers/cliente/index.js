17f8ca3bc6bd6f91b74347a6f0b49e507fa49512
JoaoFiorelli <joao.fiorelli.13@gmail.com>
2021-05-26 22:32:42 -0300

    Incluindo a validação de email e tipo sanguineo versão 1.

--- controllers/cliente/index.js
+++ controllers/cliente/index.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import db from '../../modal/cliente/index.js'
+import validationData from '../../services/validations.js'
 
 const router = express.Router();
 router.post('/', async (req,res) => {
     try{
         const {zip_code, street, number, district, locale, uf, cpf, name, phone, cellphone, email, blood_type} = req.body;
-        const blood = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
-        const valid = validation(zip_code, street, number, district, locale, uf, cpf, name, phone, cellphone, email, blood_type);
-        if (valid == true){
+        const valid = validationData.validation(email, blood_type);
+        console.log('teste')
+        if (valid){
             await db.insertClient(zip_code, street, number, district, locale, uf, cpf, name, phone, cellphone, email, blood_type);
             res.send('Cadastro realizado com sucesso!');
-        }else{
-            res.send("Erro ao cadastrar!");
+        }else if(valid == 'status1'){
+            res.send('Erro ao cadastrar o tipo sanguineo!');
+        }else if(valid == 'status2'){
+            res.send('Erro ao validar o email!')
         }
     }catch(err){
         res.send(err);
