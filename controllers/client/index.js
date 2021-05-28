6f630d5969e4a27b879c9f43b318f8e926d5c059
JoaoFiorelli <joao.fiorelli.13@gmail.com>
2021-05-27 21:14:11 -0300

    Fazendo validações iniciais com o express-validator

--- controllers/client/index.js
+++ controllers/client/index.js
@@ -1,24 +1,34 @@
 import express from 'express';
 import db from '../../modal/cliente/index.js'
-import validationData from '../../services/validations.js'
+import {body, validationResult} from 'express-validator';
 
 const router = express.Router();
-router.post('/', async (req,res) => {
-    try{
-        const {zip_code, street, number, district, locale, uf, cpf, name, phone, cellphone, email, blood_type} = req.body;
-        const valid = validationData.validation(email, blood_type);
-        console.log('teste')
-        if (valid){
-            await db.insertClient(zip_code, street, number, district, locale, uf, cpf, name, phone, cellphone, email, blood_type);
-            res.send('Cadastro realizado com sucesso!');
-        }else if(valid == 'status1'){
-            res.send('Erro ao cadastrar o tipo sanguineo!');
-        }else if(valid == 'status2'){
-            res.send('Erro ao validar o email!')
+router.post('/', [
+    body('zip_code').isLength({min: 8, max: 8}).withMessage('CEP inválido'),
+    body('zip_code').isNumeric().withMessage('Entre com um valor numérico de CEP'),
+    body('email').isEmail().withMessage('Entre com um email válido'),
+    body('blood_type').custom(blood => {
+        const blood_types_allow = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'];
+        console.log(blood)
+        if (!blood_types_allow.includes(blood)){
+            return Promise.reject('Tipo sanguíneo inválido');
         }
-    }catch(err){
-        res.send(err);
+    })
+], async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).send({errors: errors.array()})
     }
+    //const {zip_code, street, number, district, locale, uf, cpf, name, phone, cellphone, email, blood_type} = req.body;
+    //await db.insertClient(zip_code, street, number, district, locale, uf, cpf, name, phone, cellphone, email, blood_type);
+    //res.send('Cadastro realizado com sucesso!');
 });
 
 export default router;
+
+
+// TODOS: vazio
+//uf tamanho=2 e se é valido (mão)
+//phone tamanho=13
+//cellphone = 13-14
+//blood tam= 2-3 e se está no conjunto(mão)
