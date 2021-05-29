c590ae53d58f8512352653e7cf40f53c3d3210ed
LFelipe-sb <luisfelipe.sb@outlook.com>
2021-05-28 23:46:34 -0300

    oi

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import express from 'express';
 import routes from './routes.js';
 
 const app = express();
+app.get('/', (req, res) => {
+    res.send('oi')
+});
 app.use(express.json());
 app.use('/', routes);
+
 
 app.listen(3001, () => {
     console.log('Servidor online');
