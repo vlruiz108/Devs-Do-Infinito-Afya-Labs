0a4244b8f7e6fec56089dd00a470ab5810ac7210
LFelipe-sb <luisfelipe.sb@outlook.com>
2021-05-30 19:55:37 -0300

    Rota de Profissoes - Alterações e listagem.

--- src/modal/profession/index.js
+++ src/modal/profession/index.js
@@ -6,4 +6,19 @@ async function insertProfession(profession_name) {
   await conn.query(sql, profession_name);
 }
 
-export default {insertProfession};
+async function listProfession() {
+  const conn = await database.connect();
+  const [rows] = await conn.query('SELECT * FROM tbl_professions');
+  return rows;
+}
+
+async function updateProfession(profession_name, id_profession) {
+  const conn = await database.connect();
+  const sql = 'CALL sp_updateProfission(?, ?);';
+  const dataProfession = [profession_name, id_profession];
+  await conn.query(sql, dataProfession);
+}
+
+
+
+export default {insertProfession, listProfession, updateProfession};
