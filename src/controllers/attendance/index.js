3f07d08411c83fac222f244dcc997a6ad2f33815
JoaoFiorelli <joao.fiorelli.13@gmail.com>
2021-05-31 23:35:31 -0300

    Método PUT para atualização de agendamentos

--- src/controllers/attendance/index.js
+++ src/controllers/attendance/index.js
@@ -31,4 +31,36 @@ router.post('/', [
     }
 });
 
+router.put('/', [
+    body('schedule_date').isDate().withMessage('Entra com o formato data para a data de agendamento'),
+    body('attendance_date').isDate().withMessage('Entra com o formato data para a data do atendimento'),
+    body('attendance_value').isNumeric().withMessage('Entre com um valor numérico para o attendance_value'),
+    body('attendance_value').isLength({min: 1}).withMessage('Attendance_value vazio'),
+    body('attendance_status').custom(status => {
+        const status_allow = ['AGENDADO', 'REALIZADO', 'CANCELADO'];
+        if (!status_allow.includes(status)){
+            return Promise.reject('Status inválido');
+        }
+        return true;
+    })
+] , async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).send({errors: errors.array()})
+    }
+    const {schedule_date, attendance_date, attendance_value, attendance_status, FK_id_med_reg, FK_id_specialist, id_attendance} = req.body;
+    const attendances = await db.listAttendance();
+    const attendance = attendances.find(item => {
+        if (item.id_attendance == id_attendance){
+            return item;
+        }
+    })
+    try{
+        await db.updateAttendance(schedule_date, attendance_date, attendance_value, attendance_status, FK_id_med_reg, FK_id_specialist, id_attendance);
+        res.status(201).send({message: `Agendamento atualizado com sucesso!`});
+    }catch(err){
+        res.status(500).send({message: `Houve um erro no banco de dados. ${err}`})
+    }
+});
+
 export default router;
--- src/modal/attendance/index.js
+++ src/modal/attendance/index.js
@@ -7,4 +7,17 @@ async function insertAttendance(schedule_date, attendance_date, attendance_value
     await conn.query(sql, dataNewAttendance);
 }
 
-export default {insertAttendance}
+async function listAttendance() {
+    const conn = await database.connect();
+    const [rows] = await conn.query('SELECT * FROM vw_list_attendances');
+    return rows;
+}
+
+async function updateAttendance(schedule_date, attendance_date, attendance_value, attendance_status, FK_id_med_reg, FK_id_specialist, id_attendance) {
+    const conn = await database.connect();
+    const sql = 'CALL sp_updateAttendance(?, ?, ?, ?, ?, ?, ?)';
+    const dataNewAttendanceInfo = [schedule_date, attendance_date, attendance_value, attendance_status, FK_id_med_reg, FK_id_specialist, id_attendance];
+    await conn.query(sql, dataNewAttendanceInfo);
+}
+
+export default {insertAttendance, listAttendance, updateAttendance}
